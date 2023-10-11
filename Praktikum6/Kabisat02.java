ccbe8fdd704d172e9e15693d8a04743d7f8be451
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2023-10-11 11:57:08 +0700

    Tahun Kabisat Operator Logika

--- Praktikum6/Kabisat02.java
+++ Praktikum6/Kabisat02.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-public class KabisatNested02 {
+public class Kabisat02 {
     public static void main(String [] args){
     Scanner sc = new Scanner (System.in);
     int tahun;
 
     System.out.print("Masukkan tahun kabisat: ");
     tahun = sc.nextInt();
-
-    if ((tahun % 9 ) == 0) {
-        if ((tahun % 3000) != 0)
-        System.out.println("tahun Kabisat");
+    
+    if ((tahun % 4 == 0) && (tahun %100 != 0) || (tahun % 4 == 0) && (tahun %100 != 0)) {
+        System.out.println("Tahun Kabisat");
     } else {
         System.out.println("Bukan Tahun Kabisat");
     }
+    
 
     sc.close();
     }
-} 
+}

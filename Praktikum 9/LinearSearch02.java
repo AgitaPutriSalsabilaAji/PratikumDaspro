bfb7fe23dab0d4dab0e03e2bb827dd7aea4f9964
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2023-11-01 15:11:15 +0700

    Modifikasi key

--- Praktikum 9/LinearSearch02.java
+++ Praktikum 9/LinearSearch02.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 public class LinearSearch02 {
     public static void main(String[] args) {
-        int[] arrayInt = {34,18,26,48,72,20,56,63};
-        int key = 20;
-        int hasil = 0;
+    Scanner sc = new Scanner(System.in);
+        
+    int[] arrayInt = new int [6];
+    int key = 0, hasil = 0, array;
 
-        for (int i = 0; i < arrayInt.length; i++) {
+    System.out.print("Masukkan jumlah elemen array :");
+    array = sc.nextInt();
+
+    for (int i = 0; i < arrayInt.length; i++) {
+        System.out.print("Masukkan elemen array yang ke-" + (i) + ": ");
+        arrayInt[i] = sc.nextInt();
+        } System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+        for(int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.print("Key dalam array pada posisi indeks ke- " + hasil);
+                break;
+            }else{
+                System.out.print("Key tidak ditemukan");
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+
     }
 }

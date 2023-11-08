fcf2d3c6513a20c1f7df7dcf02b7aba9d82cb2f5
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2023-11-08 12:04:02 +0700

    Modifikasi Bioskop 2

--- Praktikum 10/Bioskop02.java
+++ Praktikum 10/Bioskop02.java
@@ -17,12 +17,12 @@ public class Bioskop02 {
     for (int i = 0; i < penonton.length; i++) {
         System.out.println("Panjang baris ke- " + (i+1) + ": " + penonton[i].length);
     }
-  
-    System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-    System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-    System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-    System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
+    System.out.println(penonton.length);
+
+    for (String[] barisPenonton : penonton) {
+        System.out.println("Panjang baris " + barisPenonton.length);
+    }
     System.out.println(penonton.length);
     System.out.println(penonton[0].length);
     System.out.println(penonton[1].length);

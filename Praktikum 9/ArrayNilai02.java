afb98891453ac940a8c2a53c8d6f1257cc9f8476
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2023-11-08 08:45:50 +0700

    nilai mahasiswa

--- Praktikum 9/ArrayNilai02.java
+++ Praktikum 9/ArrayNilai02.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
 public class ArrayNilai02 {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
+    int[] nilaiAkhir = new int [10];
 
-    int[] nilaiAkhir = new int[11];
-    for (int i = 1; i < 11; i++) {
-        System.out.print("Masukkan nilai akhir mahsiswa ke-" + (i) + ": ");
-    int[] nilaiAkhir = new int[10];
     for (int i = 0; i < nilaiAkhir.length; i++) {
-        System.out.print("Masukkan nilai akhir mahsiswa ke-" + (i+1) + ": ");
-        nilaiAkhir[i] = sc.nextInt();
+        System.out.print("Masukkan nilai akhir mahasiswa ke- " + (i+1) + ":");
+        nilaiAkhir[i] = input.nextInt();
     }
-    for (int i = 1; i < 11; i++) {
-        System.out.println("Nilai akhir mahasiswa ke-" + (i) + "adalah" + nilaiAkhir[i]);
     for (int i = 0; i < 10; i++) {
         if (nilaiAkhir[i] > 70) {
-            System.out.println("Mahasiswa ke-" + (i+1) + " lulus ");
-        } else if (nilaiAkhir[i] < 70) {
-            System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus ");
+            System.out.println("Mahasiswa ke- " + (i+1) + " Lulus");
+        }
+        if (nilaiAkhir[i] < 70) {
+            System.out.println("Mahasiswa ke- " + (i+1) + " Tidak Lulus");
         }
     }
-    }
 }
-    }
 }

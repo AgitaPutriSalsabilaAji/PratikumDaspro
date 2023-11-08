46884076c7971386524944218c445902d08a484e
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2023-11-08 12:18:34 +0700

    Modifikasi penonton bioskop 1

--- Praktikum 10/Bioskop02.java
+++ Praktikum 10/Bioskop02.java
@@ -1,6 +1,5 @@
 public class Bioskop02 {
     public static void main(String[] args) {
-    
     String [][] penonton = new String[4][2];
 
     penonton[0][0] = "Amin";
@@ -18,16 +17,16 @@ public class Bioskop02 {
         System.out.println("Panjang baris ke- " + (i+1) + ": " + penonton[i].length);
     }
 
+    System.out.println("Penonton pada baris ke-3: ");
+    for (int i = 0; i < penonton[2].length; i++) {
+        System.out.println(penonton[2][i]);
+    }
     System.out.println(penonton.length);
 
     for (String[] barisPenonton : penonton) {
         System.out.println("Panjang baris " + barisPenonton.length);
     }
-    System.out.println(penonton.length);
-    System.out.println(penonton[0].length);
-    System.out.println(penonton[1].length);
-    System.out.println(penonton[2].length);
-    System.out.println(penonton[3].length);
+    
 
     }
 }

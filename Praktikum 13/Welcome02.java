c4c3d1c70e14f0c6f1f591b1ca3531910921f34d
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2023-11-29 10:55:52 +0700

    Welcome

---
+++ Praktikum 13/Welcome02.java
@@ -0,0 +1,9 @@
+public class Welcome02 {
+    public static void main(String[] args) {
+        displaywelcomeMessage();
+    }
+    
+    public static void displaywelcomeMessage(){
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+    }
+}
